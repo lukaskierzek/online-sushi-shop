8bda7400f20f0005814c1f56708913a850370bf1
lukaskierzek <lukas.kierzek@gmail.com>
2025-01-14 11:06:45 +0100

    Create AdditionalInformation embeddable class

---
+++ src/main/java/com/sushiShop/onlineSushiShop/model/AdditionalInformation.java
@@ -0,0 +1,59 @@
+package com.sushiShop.onlineSushiShop.model;
+
+import com.sushiShop.onlineSushiShop.enums.IsHidden;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Enumerated;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+public class AdditionalInformation {
+
+    @Enumerated
+    @Column(nullable = false)
+    private IsHidden isHidden;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+
+    public AdditionalInformation() {
+    }
+
+    public AdditionalInformation(IsHidden isHidden, LocalDateTime createdAt, LocalDateTime updateAt) {
+        this.isHidden = isHidden;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public IsHidden getIsHidden() {
+        return isHidden;
+    }
+
+    public void setIsDeleted(IsHidden isHidden) {
+        this.isHidden = isHidden;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+}
