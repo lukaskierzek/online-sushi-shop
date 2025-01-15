44f6c82f622207e2fb1e5f0d3ab644a41d5cecd0
lukaskierzek <lukas.kierzek@gmail.com>
2025-01-15 12:02:09 +0100

    Add attributes in MainCategory class

--- src/main/java/com/sushiShop/onlineSushiShop/model/MainCategory.java
+++ src/main/java/com/sushiShop/onlineSushiShop/model/MainCategory.java
@@ -1,4 +1,13 @@
 package com.sushiShop.onlineSushiShop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "main_categories")
 public class MainCategory {
+
+    private Long mainCategoryId;
+    private String mainCategoryName;
+    private AdditionalInformation additionalInformation;
 }
