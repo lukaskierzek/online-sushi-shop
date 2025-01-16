5236496650499b67135b1e73c4ef955de53fe8be
lukaskierzek <lukas.kierzek@gmail.com>
2025-01-16 18:16:07 +0100

    Add attributes to ItemDTO

--- src/main/java/com/sushiShop/onlineSushiShop/model/dto/ItemDTO.java
+++ src/main/java/com/sushiShop/onlineSushiShop/model/dto/ItemDTO.java
@@ -1,4 +1,14 @@
 package com.sushiShop.onlineSushiShop.model.dto;
 
+import com.sushiShop.onlineSushiShop.enums.IsHidden;
+
 public class ItemDTO {
+    private Long ItemId;
+    private String ItemName;
+    private Integer ItemActualPrice;
+    private Integer ItemOldPrice;
+    private String ItemImageUrl;
+    private IsHidden ItemIsHidden;
+    private String ItemComment;
+    private String ItemMainCategory;
 }
