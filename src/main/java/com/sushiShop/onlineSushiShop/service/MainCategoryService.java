fbd836a3d18fd1d240df5e569b7e4c56a6e7ace8
lukaskierzek <lukas.kierzek@gmail.com>
2025-01-24 11:54:00 +0100

    Add MainCategoryController with all and by Id

--- src/main/java/com/sushiShop/onlineSushiShop/service/MainCategoryService.java
+++ src/main/java/com/sushiShop/onlineSushiShop/service/MainCategoryService.java
@@ -5,6 +5,8 @@ import com.sushiShop.onlineSushiShop.repository.MainCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MainCategoryService {
     private final MainCategoryRepository mainCategoryRepository;
@@ -17,5 +19,9 @@ public class MainCategoryService {
     public MainCategory getMainCategoryById(Long mainCategoryId) {
         return mainCategoryRepository.findMainCategoryById(mainCategoryId)
                 .orElseThrow(() -> new IllegalStateException(String.format("Main category by %s was not found", mainCategoryId)));
+    }
+
+    public List<MainCategory> getAllMainCategories() {
+        return mainCategoryRepository.findAll();
     }
 }
