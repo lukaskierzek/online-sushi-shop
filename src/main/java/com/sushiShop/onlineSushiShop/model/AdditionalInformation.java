8cd650093ec9457d9a9ff25aa9583efff7ad24e9
lukaskierzek <lukas.kierzek@gmail.com>
2025-02-05 11:21:17 +0100

    Refactor imports

--- src/main/java/com/sushiShop/onlineSushiShop/model/AdditionalInformation.java
+++ src/main/java/com/sushiShop/onlineSushiShop/model/AdditionalInformation.java
@@ -1,13 +1,8 @@
 package com.sushiShop.onlineSushiShop.model;
 
 import com.sushiShop.onlineSushiShop.enums.IsHidden;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Enumerated;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Embeddable
 public class AdditionalInformation {
--- src/main/java/com/sushiShop/onlineSushiShop/service/MainCategoryService.java
+++ src/main/java/com/sushiShop/onlineSushiShop/service/MainCategoryService.java
@@ -1,6 +1,5 @@
 package com.sushiShop.onlineSushiShop.service;
 
-import com.sushiShop.onlineSushiShop.enums.IsHidden;
 import com.sushiShop.onlineSushiShop.exception.MainCategoryNotFoundException;
 import com.sushiShop.onlineSushiShop.model.MainCategory;
 import com.sushiShop.onlineSushiShop.repository.MainCategoryRepository;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class MainCategoryService {
