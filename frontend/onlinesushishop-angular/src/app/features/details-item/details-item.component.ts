0990feed368264bcce92862a3a714dbaea963f5e
lukaskierzek <lukas.kierzek@gmail.com>
2025-02-11 17:43:47 +0100

    Init details-item component

---
+++ frontend/onlinesushishop-angular/src/app/features/details-item/details-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-item',
+  imports: [],
+  templateUrl: './details-item.component.html',
+  styleUrl: './details-item.component.css'
+})
+export class DetailsItemComponent {
+
+}
