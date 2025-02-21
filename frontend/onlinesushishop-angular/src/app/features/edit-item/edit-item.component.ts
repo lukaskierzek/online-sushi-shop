6bc2e75d3ea134ae647757a30e0f0f2e5d38d521
lukaskierzek <lukas.kierzek@gmail.com>
2025-02-21 12:07:48 +0100

    Generate edit-item component

---
+++ frontend/onlinesushishop-angular/src/app/features/edit-item/edit-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-item',
+  imports: [],
+  templateUrl: './edit-item.component.html',
+  styleUrl: './edit-item.component.css'
+})
+export class EditItemComponent {
+
+}
